a03901ba7477455dac574c901e4759a7d0d17411
luzede <85195923+luzede@users.noreply.github.com>
2024-01-30 15:35:41 +0200

    Completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,14 @@
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
     title: 'Customers'
 }
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page() {
+    const customers = await fetchFilteredCustomers('');
+
+
+    return <CustomersTable customers={customers}/>
 }
