c7585874677e2ce2e6aa1eb8b4824c1d26a98e61
luzede <85195923+luzede@users.noreply.github.com>
2024-01-30 17:09:14 +0200

    Completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,19 @@
 import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
-import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
     title: 'Customers'
 }
 
-export default async function Page() {
-    const customers = await fetchFilteredCustomers('');
+export default async function Page({ searchParams }: {
+    searchParams?: {
+        query?: string,
+        page?: string
+    }
+}) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
 
 
     return <CustomersTable customers={customers}/>
